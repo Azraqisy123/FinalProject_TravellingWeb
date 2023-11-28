3f0ab43fe74f5e35c73b719aa8f0158fdf94353f
Azraqisy123 <107374487+Azraqisy123@users.noreply.github.com>
2023-11-28 20:13:13 +0700

    update-fix about us

--- index.php
+++ index.php
@@ -189,12 +189,13 @@
             <h2>About Us</h2>
           </div>
         </div>
+
         <div class="container my-5">
           <div class="row">
 
             <!-- Start Column 1 -->
             <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-              <img src="/assets/images/person_1.jpg" class="img-fluid mb-5">
+              <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" class="img-fluid mb-5">
               <h3 clas><a href="#"><span class="">Lawson</span> Arnold</a></h3>
               <span class="d-block position mb-4">CEO, Founder, Atty.</span>
               <p>Separated they live in.
@@ -206,7 +207,7 @@
 
             <!-- Start Column 2 -->
             <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-              <img src="/assets/images/person_2.jpg" class="img-fluid mb-5">
+              <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t2.jpg" class="img-fluid mb-5">
 
               <h3 clas><a href="#"><span class="">Jeremy</span> Walker</a></h3>
               <span class="d-block position mb-4">CEO, Founder, Atty.</span>
@@ -220,7 +221,7 @@
 
             <!-- Start Column 3 -->
             <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-              <img src="/assets/images/person_3.jpg" class="img-fluid mb-5">
+              <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t3.jpg" class="img-fluid mb-5">
               <h3 clas><a href="#"><span class="">Patrik</span> White</a></h3>
               <span class="d-block position mb-4">CEO, Founder, Atty.</span>
               <p>Separated they live in.
@@ -232,7 +233,7 @@
 
             <!-- Start Column 4 -->
             <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-              <img src="/assets/images/person_4.jpg" class="img-fluid mb-5">
+              <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t4.jpg" class="img-fluid mb-5">
 
               <h3 clas><a href="#"><span class="">Kathryn</span> Ryan</a></h3>
               <span class="d-block position mb-4">CEO, Founder, Atty.</span>
