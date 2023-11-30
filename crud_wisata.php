5e3b7b190c01457ddce1cc06bd8f89b3967c8026
Solihudin15 <zsolihudin0215@gmail.com>
2023-11-30 18:10:36 +0700

    update

--- crud_wisata.php
+++ crud_wisata.php
@@ -116,7 +116,7 @@ $travels = mysqli_query($connection, "SELECT * FROM wisata JOIN kategori ON wisa
 
                             <h6>Rp <?php echo number_format($travel['htm']); ?></h6>
 
-                            <p class="text-justify"><?php echo substr_replace($travel['deskripsi'], " ...", 150); ?></p>
+                            <p class="text-justify"><?php echo substr_replace($travel['deskripsi'], " ...", 130); ?></p>
 
                             <small class="d-flex justify-content-between mb-4">
                                 <strong title="Nights"><i class="fa fa-cube"></i> Category: <?php echo $travel['nama_kategori']; ?></strong>
