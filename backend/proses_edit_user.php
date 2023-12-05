e28565d45cbf3ee0cadb4e05dac8aceed79fd6a9
Solihudin15 <zsolihudin0215@gmail.com>
2023-12-05 23:06:42 +0700

    Fix Upload Foto

--- backend/proses_edit_user.php
+++ backend/proses_edit_user.php
@@ -13,9 +13,18 @@
     $foto_user = $_FILES['foto_user']['name'];
     $file_tmp = $_FILES['foto_user']['tmp_name'];
     
-    move_uploaded_file($file_tmp, '../assets/images/user/' . $foto_user);
+    // JIka Upload Foto Baru
+    if (strlen($foto_user) > 0){
+        move_uploaded_file($file_tmp, '../assets/images/user/' . $foto_user);
+
+        mysqli_query($connection, "UPDATE user SET nama_user = '$nama_user', username = '$username_user', password = '$password_user', jenis_kelamin = '$jenis_kelamin', 
+        email_user = '$email_user', no_telp = '$no_telp', alamat = '$alamat', role = '$role', foto_user = '$foto_user' WHERE id_user = '$id' ");
+    }
     
-    mysqli_query($connection, "UPDATE user SET nama_user = '$nama_user', username = '$username_user', password = '$password_user', jenis_kelamin = '$jenis_kelamin', 
-    email_user = '$email_user', no_telp = '$no_telp', alamat = '$alamat', role = '$role', foto_user = '$foto_user' WHERE id_user = '$id' ");
+    // Jika Tidak Upload Foto Baru
+    else{
+        mysqli_query($connection, "UPDATE user SET nama_user = '$nama_user', username = '$username_user', password = '$password_user', jenis_kelamin = '$jenis_kelamin', 
+        email_user = '$email_user', no_telp = '$no_telp', alamat = '$alamat', role = '$role' WHERE id_user = '$id' ");
+    }
     
     header("location:../crud_user.php");
--- backend/proses_edit_wisata.php
+++ backend/proses_edit_wisata.php
@@ -20,14 +20,23 @@
 
     $foto_4 = $_FILES['foto_4']['name'];
     $file_tmp_4 = $_FILES['foto_4']['tmp_name'];
-    
-    move_uploaded_file($file_tmp_1, '../assets/images/wisata/' . $foto_1);
-    move_uploaded_file($file_tmp_2, '../assets/images/wisata/' . $foto_2);
-    move_uploaded_file($file_tmp_3, '../assets/images/wisata/' . $foto_3);
-    move_uploaded_file($file_tmp_4, '../assets/images/wisata/' . $foto_4);
-    
-    mysqli_query($connection, "UPDATE wisata SET nama_tempat = '$nama_tempat', id_kategori = '$id_kategori', lokasi = '$lokasi', fasilitas = '$fasilitas', 
-    htm = '$htm', deskripsi = '$deskripsi', foto_1 = '$foto_1', foto_2 = '$foto_2', foto_3 = '$foto_3', foto_4 = '$foto_4' WHERE id_wisata = '$id' ");
+
+    // Jika Upload Foto Baru
+    if (strlen($foto_1 && $foto_2 && $foto_3 && $foto_4) > 0){
+        move_uploaded_file($file_tmp_1, '../assets/images/wisata/' . $foto_1);
+        move_uploaded_file($file_tmp_2, '../assets/images/wisata/' . $foto_2);
+        move_uploaded_file($file_tmp_3, '../assets/images/wisata/' . $foto_3);
+        move_uploaded_file($file_tmp_4, '../assets/images/wisata/' . $foto_4);
+        
+        mysqli_query($connection, "UPDATE wisata SET nama_tempat = '$nama_tempat', id_kategori = '$id_kategori', lokasi = '$lokasi', fasilitas = '$fasilitas', 
+        htm = '$htm', deskripsi = '$deskripsi', foto_1 = '$foto_1', foto_2 = '$foto_2', foto_3 = '$foto_3', foto_4 = '$foto_4' WHERE id_wisata = '$id' ");
+    } 
+
+    // Jika Tidak Upload Foto Baru
+    else {
+        mysqli_query($connection, "UPDATE wisata SET nama_tempat = '$nama_tempat', id_kategori = '$id_kategori', lokasi = '$lokasi', fasilitas = '$fasilitas', 
+        htm = '$htm', deskripsi = '$deskripsi' WHERE id_wisata = '$id' ");
+    }
     
     header("location:../crud_wisata.php");
 ?>
